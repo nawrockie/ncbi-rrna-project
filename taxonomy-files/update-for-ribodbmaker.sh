f126f2b8732befab3ad4e1d2adbe11f09b803ca3
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-07-17 02:01:00 -0400

    Adds script to update taxonomy files for ribodbmaker

---
+++ taxonomy-files/update-for-ribodbmaker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! "$BASH_VERSION" ] ; then
+    echo "Please do not use sh to run this script ($0), just execute it directly, e.g.\n'update-for-ribodbmaker.sh'" 1>&2
+    exit 1
+fi
+
+
+if [ ! "$VECPLUSDIR" ] ; then
+    echo "The VECPLUSDIR environment variable is not set. Please set it to the directory where vecscreen_plus_taxonomy is installed, e.g.:\n'export VECPLUSDIR=/PATH-TO-VECPLUSTAXONOMY/'" 1>&2
+    exit 1
+fi
+
+cp /am/ftp-private/ncbitax/taxdump_new.tar.gz .
+gunzip -f taxdump_new.tar.gz
+tar xf taxdump_new.tar
+cut -f1,3,5 nodes.dmp > taxonomy_tree.txt
+cut -f31 nodes.dmp > specified_column.txt
+$VECPLUSDIR/scripts/assign_levels_to_taxonomy.pl --input_taxa taxonomy_tree.txt --outfile taxonomy_tree_wlevels.txt
+paste taxonomy_tree_wlevels.txt specified_column.txt > taxonomy_tree_ribodbmaker.txt
